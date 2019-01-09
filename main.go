d796d97821ad7c1f8e53c1dc144835b9dcb95178
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2019-01-10 01:11:10 +0300

    changed to user-show

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/url"
 	"os"
 
 	"github.com/ChimeraCoder/anaconda"
@@ -12,17 +11,25 @@ import (
 func main() {
 
 	api := anaconda.NewTwitterApiWithCredentials(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"), os.Getenv("API_KEY"), os.Getenv("API_SECRET"))
-	v := url.Values{}
-	v.Set("count", "1")
-	v.Set("screen_name", "YavuzSubasi")
-	tweets, err := api.GetUserTimeline(v)
+	user, err := api.GetUsersShow(os.Args[1], nil)
 	if err != nil {
 		fmt.Println(err)
 	}
-	b, err := json.Marshal(tweets)
-	fmt.Println(string(b))
-	// for _, tweet := range tweets {
-	// 	fmt.Println(tweet.Text)
-	// }
-
+	userjson, err := json.Marshal(user)
+	fmt.Println(string(userjson))
+	fmt.Println("--------------------")
 }
+
+/*
+description
+statuses_count
+verified
+favourites_count
+followers_count
+friends_count
+listed_count
+location
+name
+profile_background_image_url_https
+profile_image_url_https
+*/
