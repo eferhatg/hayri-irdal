096b2173ec73f5c877e07351e44f5033d9d173b0
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2019-01-12 04:17:36 +0300

    added db layer

--- cmd/tbmm/main.go
+++ cmd/tbmm/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	log "github.com/Sirupsen/logrus"
+	"github.com/eferhatg/hayri-irdal/pkg/models"
 	"github.com/eferhatg/hayri-irdal/pkg/scrapers"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -10,8 +16,20 @@ const (
 )
 
 func main() {
+	initDB()
+}
+
+func initDB() {
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_DATABASE"), os.Getenv("DB_PASSWORD")))
+	if err != nil {
+		fmt.Print("can't initialize db", err)
+	}
+	db.AutoMigrate(&models.Deputy{})
 
 	log.SetLevel(log.DebugLevel)
 	deplist := scrapers.CrawlTBMMDeputyList(deputyListPage)
-	log.Info(len(deplist))
+	for _, d := range deplist {
+		d.Upsert(db)
+	}
+	log.Info(len(deplist), "deputy is added")
 }
