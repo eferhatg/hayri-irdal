c12ca1e715c061d83c53ffa33689c6b77394bca1
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2019-01-16 23:49:12 +0300

    worked eventually

--- cmd/tbmm/main.go
+++ cmd/tbmm/main.go
@@ -12,24 +12,40 @@ import (
 )
 
 const (
-	deputyListPage = "https://www.tbmm.gov.tr/develop/owa/milletvekillerimiz_sd.liste"
+	deputyListPage    = "https://www.tbmm.gov.tr/develop/owa/milletvekillerimiz_sd.liste"
+	deputyProfileRoot = "https://www.tbmm.gov.tr/develop/owa/milletvekillerimiz_sd.bilgi"
 )
 
 func main() {
-	initDB()
-}
-
-func initDB() {
 	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_DATABASE"), os.Getenv("DB_PASSWORD")))
 	if err != nil {
 		fmt.Print("can't initialize db", err)
 	}
-	db.AutoMigrate(&models.Deputy{})
 
+	db.AutoMigrate(&models.Deputy{})
 	log.SetLevel(log.DebugLevel)
-	deplist := scrapers.CrawlTBMMDeputyList(deputyListPage)
+
+	//fetchTBMMDeputyList(db, deputyListPage)
+	fetchDeputyProfiles(db)
+}
+
+func fetchTBMMDeputyList(db *gorm.DB, deputyListPage string) {
+	deplist := scrapers.ScrapeTBMMDeputyList(deputyListPage)
 	for _, d := range deplist {
 		d.Upsert(db)
 	}
 	log.Info(len(deplist), "deputy is added")
+}
+
+func fetchDeputyProfiles(db *gorm.DB) {
+	deputyList := []models.Deputy{}
+	db.Find(&deputyList)
+	for index := 0; index < len(deputyList); index++ {
+		d := deputyList[index]
+		logr := log.WithFields(log.Fields{"action": "update profile", "Deputy": d})
+		updatedDeputy := scrapers.ScrapeDeputyProfilePage(d, deputyProfileRoot)
+		db.Save(&updatedDeputy)
+		logr.Debug("Deputy profile updated", updatedDeputy)
+	}
+
 }
--- pkg/scrapers/tbmm.go
+++ pkg/scrapers/tbmm.go
@@ -1,8 +1,10 @@
 package scrapers
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
+	"time"
 	"unicode"
 
 	"github.com/PuerkitoBio/goquery"
@@ -11,7 +13,7 @@ import (
 	"github.com/eferhatg/hayri-irdal/pkg/utils"
 )
 
-func CrawlTBMMDeputyList(deputyListPage string) []models.Deputy {
+func ScrapeTBMMDeputyList(deputyListPage string) []models.Deputy {
 	logr := log.WithFields(log.Fields{"action": "Crawling deputy"})
 	doc := utils.GetDocument(deputyListPage)
 
@@ -38,6 +40,90 @@ func CrawlTBMMDeputyList(deputyListPage string) []models.Deputy {
 	return deputies
 }
 
+//CrawlDeputyProfilePage crawl and find out deputy profile details
+func ScrapeDeputyProfilePage(d models.Deputy, deputyProfileRoot string) models.Deputy {
+	logr := log.WithFields(log.Fields{"action": "Crawling deputy profile"})
+	doc := utils.GetDocument(deputyProfileRoot + "?p_donem=" + strconv.Itoa(d.TermNo) + "&p_sicil=" + strconv.Itoa(d.DeputyNo))
+	logr.Info(d.Name, d.Surname)
+	if strings.Split(doc.Find("div#mv_ili").Text(), " ")[0] == "" {
+		logr.Debug("Deputy not found", d)
+		d.IsActive = false
+		return d
+
+	}
+
+	doc.Find("div#iletisim_bilgi table tbody tr").EachWithBreak(func(i int, tr *goquery.Selection) bool {
+		tdArr := tr.Find("td")
+		if tdArr.Length() > 1 && tdArr.Find("span").Length() != 0 {
+			fc := tdArr.Get(0).FirstChild.Data
+			sc := tdArr.Get(1).FirstChild.Data
+			if fc != "span" || sc != "span" {
+				return false
+			}
+			key := tdArr.Get(0).FirstChild.FirstChild.Data
+			val := tdArr.Get(1).FirstChild.NextSibling.Data
+			if key == "Adres" {
+				d.Address = strings.TrimSpace(val)
+			} else if key == "Telefon" {
+				d.Tel = strings.TrimSpace(val)
+			} else if key == "Faks" {
+				d.Fax = strings.TrimSpace(val)
+			} else if key == "E-Posta" {
+				d.Email = strings.TrimSpace(tdArr.Get(1).LastChild.FirstChild.Data)
+			} else if key == "Web" {
+				d.Web = strings.TrimSpace(tdArr.Get(1).LastChild.FirstChild.Data)
+			} else {
+
+				fmt.Printf("%s\n", strings.TrimSpace(tdArr.Get(1).FirstChild.Data))
+
+				tdArr.Last().Find("a").Each(func(k int, a *goquery.Selection) {
+					href, ext := a.Attr("href")
+					if ext {
+						if strings.Contains(href, "facebook.com") {
+							d.FbLink = href
+						}
+						if strings.Contains(href, "twitter.com") {
+							d.TwLink = href
+						}
+					}
+				})
+
+			}
+
+		}
+		return true
+
+	})
+
+	d.Region = strings.Split(doc.Find("div#mv_ili").Text(), " ")[2]
+
+	d.ProfilePic, _ = doc.Find("div#fotograf_alani img").Attr("src")
+
+	divGorev := doc.Find("div#mv_gorev")
+	position := ""
+	for index := 0; index < divGorev.Children().Length(); index++ {
+		pos := strings.TrimSpace(divGorev.Children().Get(index).NextSibling.Data)
+		if len(pos) > 0 {
+			position += pos + "||"
+			fmt.Printf("%+v\n", pos)
+		}
+
+	}
+	d.Position = strings.TrimSuffix(position, "||")
+
+	divCv := doc.Find("div#ozgecmis_icerik")
+
+	cv := strings.TrimSpace(divCv.Text())
+	d.BirthDay = parseBirthDate(cv)
+	d.BirthPlace = parseBirtPlace(cv)
+	d.Profession = parseProffesion(cv)
+
+	d.Cv = cv
+
+	logr.Debug("Deputy profile crawled", d)
+	return d
+}
+
 func parseFullName(fullname string) (name string, surname string) {
 
 	nameArr := strings.Split(fullname, " ")
@@ -56,4 +142,96 @@ func parseFullName(fullname string) (name string, surname string) {
 		}
 	}
 	return strings.TrimSuffix(name, " "), strings.TrimSuffix(surname, " ")
+}
+
+func parseBirthDate(cv string) time.Time {
+	m := make(map[string]string)
+	m["Ocak"] = "01"
+	m["Şubat"] = "02"
+	m["Mart"] = "03"
+	m["Nisan"] = "04"
+	m["Mayıs"] = "05"
+	m["Haziran"] = "06"
+	m["Temmuz"] = "07"
+	m["Ağustos"] = "08"
+	m["Eylül"] = "09"
+	m["Ekim"] = "10"
+	m["Kasım"] = "11"
+	m["Aralık"] = "12"
+
+	delim := [8]string{"'de", "'da", "'te", "'ta", "?de", "?da", "?te", "?ta"}
+	tc := time.Time{}
+	for _, d := range delim {
+		s := strings.Split(cv, d)
+		dp := strings.Split(s[0], " ")
+		if len(dp) == 3 {
+			dpi, err := strconv.Atoi(dp[0])
+			if err != nil {
+				break
+			}
+			bd := dp[2] + "-" + m[dp[1]] + "-" + fmt.Sprintf("%02d", dpi)
+			fmt.Println(bd)
+			tc, _ = time.Parse("2006-01-02", bd)
+			break
+		}
+	}
+	return tc
+}
+
+func parseBirtPlace(cv string) string {
+
+	pIx := -1
+
+	places := [81]string{"Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin",
+		"Aydın", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale",
+		"Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir",
+		"Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", "Mersin", "İstanbul", "İzmir",
+		"Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya",
+		"Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya",
+		"Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak",
+		"Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak",
+		"Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"}
+	sIx := 2000000000
+	for index, p := range places {
+
+		x := strings.Index(cv, p)
+
+		if x != -1 && x < sIx {
+			sIx = x
+			pIx = index
+		}
+	}
+	if pIx == -1 {
+
+		return ""
+	}
+	return places[pIx]
+}
+
+func parseProffesion(cv string) string {
+
+	pLine := strings.Split(cv, "\n")
+	if len(pLine) > 1 {
+		pBlock := strings.Split(pLine[1], ";")[0]
+		if len(pBlock) > 220 {
+			return pBlock[:220]
+		}
+		return pBlock
+	}
+	return ""
+}
+
+func parseUniversity(cv string) string {
+
+	pLine := strings.Split(cv, "\n")
+	if len(pLine) > 1 {
+		pBlock := strings.Split(pLine[1], ";")[1]
+		pUniList := strings.Split(pBlock, "Üniver")
+		if len(pUniList) > 1 {
+			return pBlock[:220]
+		}
+		return pUniList[0]
+
+	}
+	return ""
 }
