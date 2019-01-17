9272e00d818ac215c02f0eae3e54b47fb6732eae
Eyüp Ferhat GÜDÜCÜ <eferhatg@gmail.com>
2019-01-17 17:05:23 +0300

    added twitter api fetcher

--- cmd/tw/main.go
+++ cmd/tw/main.go
@@ -1,35 +1,47 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/ChimeraCoder/anaconda"
+	log "github.com/Sirupsen/logrus"
+	"github.com/eferhatg/hayri-irdal/pkg/models"
+	"github.com/eferhatg/hayri-irdal/pkg/twitter"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func main() {
 
 	api := anaconda.NewTwitterApiWithCredentials(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"), os.Getenv("API_KEY"), os.Getenv("API_SECRET"))
-	user, err := api.GetUsersShow(os.Args[1], nil)
+	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_DATABASE"), os.Getenv("DB_PASSWORD")))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print("can't initialize db", err)
 	}
-	userjson, err := json.Marshal(user)
-	fmt.Println(string(userjson))
+
+	db.AutoMigrate(&models.Deputy{})
+	log.SetLevel(log.DebugLevel)
+	fetchUserProfiles(api, db)
 	fmt.Println("--------------------")
 }
 
-/*
-description
-statuses_count
-verified
-favourites_count
-followers_count
-friends_count
-listed_count
-location
-name
-profile_background_image_url_https
-profile_image_url_https
-*/
+func fetchUserProfiles(api *anaconda.TwitterApi, db *gorm.DB) {
+	deputyList := []models.Deputy{}
+	db.Where("tw_link != ?", "").Find(&deputyList)
+	if len(deputyList) == 0 {
+		log.Info("Not found available deputy")
+	}
+	for index := 0; index < len(deputyList); index++ {
+		log.Info("Getting twitter profile of ", deputyList[index].TwLink)
+		twd := twitter.TwDeputy{Deputy: &deputyList[index]}
+		err := twd.GetUserProfile(api)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		db.Save(&twd.Deputy)
+		log.Info("Saved ", twd.Deputy.TwLink)
+
+	}
+}
---
+++ pkg/twitter/deputy.go
@@ -0,0 +1,36 @@
+package twitter
+
+import (
+	"time"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/eferhatg/hayri-irdal/pkg/models"
+)
+
+type TwDeputy struct {
+	Deputy *models.Deputy
+}
+
+func (twd *TwDeputy) GetUserProfile(api *anaconda.TwitterApi) error {
+	user, err := api.GetUsersShow(twd.Deputy.TwLink, nil)
+	if err != nil {
+
+		return err
+	}
+	timeLayout := "Mon Jan 02 15:04:05 -0700 2006"
+
+	twd.Deputy.TwDescription = user.Description
+	twd.Deputy.TwFavouritesCount = user.FavouritesCount
+	twd.Deputy.TwFollowersCount = user.FollowersCount
+	twd.Deputy.TwFriendsCount = user.FriendsCount
+	twd.Deputy.TwListedCount = user.ListedCount
+	twd.Deputy.TwLocation = user.Location
+	twd.Deputy.TwName = user.Name
+	twd.Deputy.TwProfileBackgroundImage = user.ProfileBackgroundImageUrlHttps
+	twd.Deputy.TwProfileImage = user.ProfileImageUrlHttps
+	twd.Deputy.TwStatusCount = user.StatusesCount
+	twd.Deputy.TwVerified = user.Verified
+	twd.Deputy.TwCreatedAt, _ = time.Parse(timeLayout, user.CreatedAt)
+	return nil
+
+}
